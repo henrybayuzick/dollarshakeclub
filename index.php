3636668171a735541676285fe8ab53352addb643
Henry Bayuzick <henrybayuzick@gmail.com>
2014-07-06 22:36:15 -0400

    Added share buttons and logo

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="js/base.min.js"></script>
 
+		<meta property="og:site_name" content="Dollar Shake Club"/>
+		<meta property="og:description" content="Protein shakes for just a $1/day. Click here to find out more!"/>
+		<meta property="og:image" content="http://dollarshake.club/images/facebook.jpg"/>
+		
 	</head>
 	<body>
 		<?php	
@@ -40,11 +44,19 @@
 			<div class="md-content">
 				<h1>Thank you!</h1>
 				<p>Thanks for signing up for Dollar Shake Club. You'll be the first to know when we launch. To save an <b>additional 10%</b> simply share this link with your friends and family!</p>
-				<a href='http://dollarshake.club?refer=<?php echo $referral; ?>'>http://dollarshake.club?refer=<span id='refer'><?php echo $referral; ?></span></a>
+				<a class="referral-link" href='http://dollarshake.club/?refer=<?php echo $referral; ?>'>http://dollarshake.club/?refer=<span id='refer'><?php echo $referral; ?></span></a>
+				<div class="share-buttons">
+					<a href="https://twitter.com/intent/tweet?via=dollarshakeclub&url=http://dollarshake.club/?refer=<?php echo $referral; ?>&text=Protein shakes for just a $1/day! Find out more at:&hashtags=dollarshakeclub" class="share-button twitter">Tweet</a>
+					<a href="https://www.facebook.com/sharer/sharer.php?u=http://dollarshake.club/?refer=<?php echo $referral; ?>" class="share-button fb">Share</a>
+				</div>
 			</div>
 		</div>
 		<div id="page">
-			<header></header>
+			<header>
+				<svg width="342" height="35">
+					<image xlink:href="images/logo.svg" src="images/logo.png" width="342" height="35" />
+				</svg>
+			</header>
 			<section id="grab" class="grab">
 				<h1>We put muscles<br/>on your body</h1>
 				<h2>Protein shakes delivered<br/>to your door for $1/day.</h2>
