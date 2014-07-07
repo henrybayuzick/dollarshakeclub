fa7fdbfc1e160062d7fa139c99d4db780ab99552
Zachary Feuerstein <zackfeuerstein@gmail.com>
2014-07-06 20:21:46 -0400

    Actually made referal Count work

--- email.php
+++ email.php
@@ -14,48 +14,44 @@
 	$refer = $_POST['refer'];
 	$referal_link = $_POST['referal_link'];
 
-	if($referal_link){
-		echo "There is a referal";
-
-		$getReferalCount = $mysqli->prepare("SELECT referal_count FROM Emails WHERE referal = ?");
-		$getReferalCount->bind_param("s", $referal_link);
-
-		if($getReferalCount->execute()){
-				$getReferalCount->bind_result($referalCount);
-				echo "got Referal count";
-		}
-		
-		$referalCount++;
-
-		$addToReferalCount = $mysqli->prepare("UPDATE Emails SET referal_count = ? WHERE referal = ?");
-		$addToReferalCount->bind_param("ds", $referalCount, $referal_link);
-
-		$addToReferalCount->execute();
-		echo "set referalCount";
-	}
-
-
 	if ($email) {
 		$stmt = $mysqli->prepare("INSERT INTO Emails VALUES(?, 0, ?, 0)");
 
 		$stmt->bind_param("ss", $email, $refer);
 
 		if($stmt->execute()){
-			echo "Success";	
-		} else {
-			echo "Query failed";
-		} 
-	} else {
-		echo "No email";
+
+		}
+
 	}
-?>
 	
 
+	$stmt->close();
 
+	if($referal_link){
 
+		$ref = (string)$referal_link;
 
+		$getReferalCount = $mysqli->prepare("SELECT referal_count FROM Emails WHERE referal = ?");
+		$getReferalCount->bind_param("s", $ref);
 
+		if($getReferalCount->execute()){
+			$getReferalCount->bind_result($referalCount);
+			while($getReferalCount->fetch()){
+			}	
+		}
+		
+		$referalCount++;	
 
+		$getReferalCount->close();
 
+		$setRefCount = $mysqli->prepare("UPDATE Emails SET referal_count = ? WHERE referal = ?");
+		$setRefCount->bind_param("ds", $referalCount, $ref);
+		
+		if($setRefCount->execute()){
+		}				
 
+ 	}
 
+?>
+	
